3b25c342c4e1bbfd2ab50ef26a05fe5124b686f6
Donagh Horgan <donagh@donagh-XPS-8300.(none)>
2012-10-12 23:12:29 +0100

    Added install file.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SCRIPT_NAME=`basename "$0"`
+SCRIPT_PATH=${0%`basename "$0"`}
+OLD_PATH="/home/${USER}/.local/share/rhythmbox/plugins/opencontainingfolder/"
+PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/OpenContainingFolder/"
+
+rm -rf $OLD_PATH
+rm -rf $PLUGIN_PATH
+
+mkdir -p $PLUGIN_PATH
+cp -r "${SCRIPT_PATH}"* "$PLUGIN_PATH"
+rm "${PLUGIN_PATH}${SCRIPT_NAME}"
+
